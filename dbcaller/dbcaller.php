e05a6fc9408bbe17f567ef47db2f5d0de74d43cf
JosephRoyalEagle <valkyria12345lynne@zohomail.com>
2025-05-17 13:05:21 +0000

    Royal

--- dbcaller/dbcaller.php
+++ dbcaller/dbcaller.php
@@ -1,6 +1,6 @@
 <?php
 try{
-	//$pdo = new PDO ('mysql:host=185.98.131.176;dbname=probi2118503;charset=UTF8', 'probi2118503', 'wnupbt3f7s');
+	//$pdo = new PDO ('mysql:host=localhost;dbname=cp2118503p21_appro;charset=UTF8', 'cp2118503p21_irmael', 'Wz8Eprj$V(o%');
 	$db_host = new PDO ('mysql:host=localhost;dbname=midevsoft;charset=utf8', 'root', '');
 }
 catch(Exception $e)
